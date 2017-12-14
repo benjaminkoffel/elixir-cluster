40f1641856f608c70f3a79964e733db3177b7198
Benjamin Koffel <benjaminkoffel@gmail.com>
2017-12-14 16:28:30 +1100

    update aws server

--- config/config.exs
+++ config/config.exs
@@ -4,15 +4,15 @@ config :logger,
   backends: [:console],
   compile_time_purge_level: :info
 
+config :ex_statsd,
+  host: "127.0.0.1",
+  port: 8125,
+  namespace: "elixir-cluster",
+  tags: []
+
 config :ex_vmstats,
   namespace: "vm_stats",
   backend: :ex_statsd,
   interval: 3000,
   use_histogram: true,
   sched_time: false
-
-config :ex_statsd,
-  host: "127.0.0.1",
-  port: 8125,
-  namespace: "elixir-cluster",
-  tags: []
--- lib/server.ex
+++ lib/server.ex
@@ -4,63 +4,52 @@ defmodule App.Server do
   # we're going to use name for n
   def start_link(name) do
     Logger.info "App.Server.start_link"
-    GenServer.start_link(__MODULE__, name)
+    GenServer.start_link(__MODULE__, [name, name])
   end
 
   # initialize server and start work
-  def init(state) do
+  def init([name, delay]) do
     Logger.info "App.Server.init"
     Process.send_after(self(), :work, 100) # kickoff work process
-    {:ok, state}
+    {:ok, {name, delay}, 0}
   end
 
   # swarm: begin handoff
-  def handle_call({:swarm, :begin_handoff}, _from, state) do
-    Logger.info "App.Server.handle_call :swarm, :begin_handoff #{state}"
-    {:reply, {:resume, state}, state}
+  def handle_call({:swarm, :begin_handoff}, _from, {name, delay}) do
+    Logger.info "App.Server.handle_call :swarm, :begin_handoff #{name}"
+    {:reply, {:resume, delay}, {name, delay}}
   end
 
   # swarm: end handoff
-  def handle_cast({:swarm, :end_handoff, state}, _state) do
-    Logger.info "App.Server.handle_cast :swarm, :end_handoff #{state}"
-    Process.send_after(self(), :work, 100) # restart work process
-    {:noreply, state}
+  def handle_cast({:swarm, :end_handoff, delay}, {name, _}) do
+    Logger.info "App.Server.handle_cast :swarm, :end_handoff #{name}"
+    {:noreply, {name, delay}}
   end
 
   # swarm: resolve conflict
-  def handle_cast({:swarm, :resolve_conflict, _state}, state) do
-    Logger.info "App.Server.handle_cast :swarm, :resolve_conflict #{state}"
-    {:noreply, state}
+  def handle_cast({:swarm, :resolve_conflict, _delay}, {name, delay}) do
+    Logger.info "App.Server.handle_cast :swarm, :resolve_conflict #{name}"
+    {:noreply, {name, delay}}
   end
 
   # swarm: timeout
-  def handle_info(:timeout, state) do
-    Logger.info "App.Server.handle_cast :timeout #{state}"
-    {:noreply, state}
+  def handle_info(:timeout, {name, delay}) do
+    Logger.info "App.Server.handle_cast :timeout #{name}"
+    {:noreply, {name, delay}}
   end
 
   # swarm: die
-  def handle_info({:swarm, :die}, state) do
-    Logger.info "App.Server.handle_cast :swarm :die #{state}"
-    {:stop, :shutdown, state}
+  def handle_info({:swarm, :die}, {name, delay}) do
+    Logger.info "App.Server.handle_cast :swarm :die #{name}"
+    {:stop, :shutdown, {name, delay}}
   end
 
   # calculate something for value stored in state, then die
-  def handle_info(:work, state) do
-    Logger.info "App.Server.handle_info :work #{state}"
-    # a = fib(1, 1, state)
-    a = Enum.join(prime(state), ",")
-    # a = Enum.join(n_primes(state), ",")
-    Logger.info "App.Server.handle_info :work #{state} #{a}"
-    {:stop, :normal, state}
-  end
-
-  # calculate fibonacci for n
-  def fib(a, b, n) do
-    case n do
-      0 -> a
-      _ -> fib(b, a+b, n-1)
-    end
+  def handle_info(:work, {name, delay}) do
+    Logger.info "App.Server.handle_info :work #{name}"
+    a = Enum.join(prime(delay), ",")
+    Logger.info "App.Server.handle_info :work #{name} #{a}"
+    {:stop, :normal, {name, delay}}
   end
 
   # calculate n primes
